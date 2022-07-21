abddb6740f45f69357157321adee1c7a177bd320
Erik <erik95harutyunyan@gmail.com>
2022-07-21 18:58:50 +0400

    first commit

---
+++ users/users.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const userRouter = express.Router();
+
+userRouter.get('/login', (req: Request, res: Response) => {
+  res.send('login');
+});
+
+userRouter.post('/register', (req: Request, res: Response) => {
+  res.send('register');
+});
+
+export { userRouter };
